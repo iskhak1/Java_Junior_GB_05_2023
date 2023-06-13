6f1628f30a1bbd723c2b9cca407ba7ecad57288c
Iskhak Mallaev <isakasi12242001@gmail.com>
2023-06-13 11:10:19 +0300

    f

---
+++ Java_Core/src/main/java/ru/gb/lesson_4/homework/Program.java
@@ -0,0 +1,18 @@
+package ru.gb.lesson_4.homework;
+
+import ru.gb.lesson_4.homework.exceptions.MyArrayDataException;
+
+
+public class Program {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public void checkException(String [][][][] array) {
+
+    }
+
+
+}
