a69571f2ed941f2e023298282ef2e046306583e8
Iskhak Mallaev <isakasi12242001@gmail.com>
2023-06-13 18:24:50 +0300

    homework well done!

---
+++ Java_Core/src/main/java/ru/gb/lesson_4/homework/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.lesson_4.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        String [][] str = new String[][]{{"r","1","2",},{"1","1","1"}};
+        Program program = new Program();
+        program.checkException(str);
+    }
+}
